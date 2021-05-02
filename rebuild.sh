a0e32d4204fb29b7ee2dd36134ca84bc5b627646
Tobias Küchel <t.kuechel@humboldt-ka.de>
2021-05-02 19:33:41 +0200

    linuxmuster-apt-cacher-ng: debian:stable-slim

--- rebuild.sh
+++ rebuild.sh
@@ -9,6 +9,7 @@ docker inspect debian:${VERSION} | grep RepoTags -A 3
 git commit -a -m"linuxmuster-apt-cacher-ng: debian:${VERSION}" 
 
 git_log=$(git log --oneline | head -1 | cut -d " " -f 1)
+docker tag humbihupf/linuxmuster-apt-cacher-ng:latest humbihupf/linuxmuster-apt-cacher-ng:previous
 echo "Press enter to build with: docker build $buildoptions -t humbihupf/linuxmuster-apt-cacher-ng:${VERSION}-$git_log ."
 read
 docker build $buildoptions -t humbihupf/linuxmuster-apt-cacher-ng:$VERSION-$git_log .
@@ -16,11 +17,8 @@ docker build $buildoptions -t humbihupf/linuxmuster-apt-cacher-ng:$VERSION-$git_
 docker tag humbihupf/linuxmuster-apt-cacher-ng:$VERSION-$git_log humbihupf/linuxmuster-apt-cacher-ng:latest
 [ $? -ne 0 ] && echo error && exit 1
 
-echo "Try if :latest works for you. Then press enter to tag working and upload using"
-echo "docker tag humbihupf/linuxmuster-apt-cacher-ng:$VERSION-$git_log humbihupf/linuxmuster-apt-cacher-ng:working"
 echo "docker push humbihupf/linuxmuster-apt-cacher-ng:latest"
 echo "docker push humbihupf/linuxmuster-apt-cacher-ng:$VERSION-$git_log"
-docker tag humbihupf/linuxmuster-apt-cacher-ng:$VERSION-$git_log humbihupf/linuxmuster-apt-cacher-ng:working
 [ $? -ne 0 ] && echo error && exit 1
 docker push humbihupf/linuxmuster-apt-cacher-ng:latest
 [ $? -ne 0 ] && echo error && exit 1
